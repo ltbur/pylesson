6431a74e5c8a2f53b583d35fe29ef0183f57d2f5
ltbur <ltburtonst@gmail.com>
2022-07-06 14:20:11 +0900

    ファイル追加

---
+++ day4/scraping/sc1.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+res=requests.get('https://joytas.net/kaba/')
+res.encoding=res.apparent_encoding
+
+soup=BeautifulSoup(res.text,'html.parser')
+
+print(soup)
+ele=soup.find('title') #<title>コビトカバ</title>
+
+print(ele.text)
+
+imgs=soup.find_all('img')
+for img in imgs:
+    print(img.get('src'))
+
+div=soup.find(id='headerImageBox')
+
+imgs=soup.select('.headerImage')
+for img in imgs:
+    print(img.get('src'))
+
+
+names=soup.select('tr td:first-child')
+for name in names:
+    print(name.text)
