fdd76944af6391e1a2d336955c81005944611f0c
ltbur <ltburtonst@gmail.com>
2022-07-12 09:55:17 +0900

    ファイル作成

---
+++ day5/pdlesson2.py
@@ -0,0 +1,38 @@
+import pandas as pd
+df=pd.read_csv('test.csv')
+
+#条件に合うデータを抽出する
+data_s=df[df['国語'] >= 90]
+print(data_s)
+
+#複数条件(&,|,~を使う)
+data_s=df[(df['国語'] >= 90) & (df['数学'] >=80)]
+print(data_s)
+
+#集計
+print('数学の最高点:',df['数学'].max())
+print('数学の最低点:',df['数学'].min())
+print('数学の平均点:',df['数学'].mean())
+print('数学の中央値:',df['数学'].median())
+print('数学の合計:',df['数学'].sum())
+
+#データのソート
+math_df=df.sort_values('数学')
+print(math_df)
+kokugo_df=df.sort_values('国語',ascending=False)
+print(kokugo_df)
+
+#行と列の入れ替え
+df_T=df.T
+print(df_T)
+
+#データをリスト化
+li=df.values
+print(li)
+
+#csvに出力
+kokugo_df.to_csv("export1.csv")
+#インデックスを削除
+kokugo_df.to_csv("export2.csv",index=False)
+#インデックスとヘッダー削除
+kokugo_df.to_csv("export3.csv",index=False,header=False)
